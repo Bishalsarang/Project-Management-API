2958e38efc614ff0cf874a847e9c5d1f3c5f51a5
Bishalsarang <bishalsarang@gmail.com>
2020-08-24 10:52:53 +0545

    Complete initial migration schem in knex

--- src/migrations/20200824100102_initial.js
+++ src/migrations/20200824100102_initial.js
@@ -1,5 +1,7 @@
 const { tableName } = require('../constants');
 
+const MAX_TITlE_LENGTH = 50;
+
 exports.up = async function (knex) {
   // Create Users Table
   await knex.schema.createTable(tableName.users, (table) => {
@@ -12,18 +14,77 @@ exports.up = async function (knex) {
     table.enu('role', ['admin', 'project_manager', 'engineer', 'team_leader']);
   });
 
-//   Create Project table
-  await knex.schema.createTable(tableName.users, (table) => {
-   table.increments().primary();
-   table.text('title', 50).notNullable();
-   table.text('description').notNullable();
-   table.integer('manager_id').unsigned().notNullable();
+  //   Create Project table
+  await knex.schema.createTable(tableName.projects, (table) => {
+    table.increments().primary();
+    table.text('title', MAX_TITlE_LENGTH).notNullable();
+    table.text('description').notNullable();
+    table.integer('manager_id').unsigned().notNullable();
+    table.timestamp('created_at').defaultTo(knex.fn.now());
 
-   table.foreign('manager_id').references('id').inTable(tableName.users);
-   
-   table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.foreign('manager_id').references('id').inTable(tableName.users).onDelete('cascade');
+  });
+
+  //    Create members table
+  await knex.schema.createTable(tableName.members, (table) => {
+    table.increments().primary();
+
+    table.integer('user_id').notNullable();
+    table.integer('project_id').notNullable();
+
+    table.foreign('user_id').references('id').inTable(tableName.users).onDelete('cascade');
+    table.foreign('project_id').references('id').inTable(tableName.projects).onDelete('cascade');
+  });
+
+  //    Create tasks table
+  await knex.schema.createTable(tableName.tasks, (table) => {
+    table.increments().primary();
+
+    table.integer('assignee_id').notNullable();
+    table.integer('project_id').notNullable();
+    table.text('title', MAX_TITlE_LENGTH).notNullable();
+    table.text('description').notNullable();
+    table.datetime('deadline').notNullable();
+
+    table.foreign('assignee_id').references('id').inTable(tableName.users).onDelete('cascade');
+    table.foreign('project_id').references('id').inTable(tableName.projects).onDelete('cascade');
+  });
+
+  //   Create commments table
+  await knex.schema.createTable(tableName.comments, (table) => {
+    table.increments().primary();
+
+    table.integer('task_id').notNullable();
+    table.integer('commenter_id').notNullable();
+    table.text('description').notNullable();
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+
+    table.foreign('task_id').references('id').inTable(tableName.tasks).onDelete('cascade');
+    table.foreign('commenter_id').references('id').inTable(tableName.users).onDelete('cascade');
+  });
+
+  //   Create tags table
+  await knex.schema.createTable(tableName.tags, (table) => {
+    table.increments().primary();
+
+    table.integer('user_id').notNullable();
+    table.integer('task_id').notNullable();
+
+    table.foreign('task_id').references('id').inTable(tableName.tasks).onDelete('cascade');
+    table.foreign('user_id').references('id').inTable(tableName.users).onDelete('cascade');
+  });
 };
 
 exports.down = async function (knex) {
-  await knex.schema.dropTable(tableName.users);
+  // Since knex doesn't allow delete CASCADE, we can either use raw sql or delete in order of dependency
+  await Promise.all(
+    [
+      tableName.tags,
+      tableName.comments,
+      tableName.tasks,
+      tableName.members,
+      tableName.projects,
+      tableName.users
+    ].map((table) => knex.schema.dropTable(table))
+  );
 };
